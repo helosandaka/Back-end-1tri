d4eeb1c037b35c0420f2e8e6457e7ef04943aa4e
helosandaka <163142840+helosandaka@users.noreply.github.com>
2025-03-18 11:10:25 -0300

    Add files via upload

---
+++ lacos.js
@@ -0,0 +1,18 @@
+let custos = [ 1200, 900, 400, 500];
+let contador = 0; 
+let somatorio = 0;
+while (contador<custos.length){
+    somatorio = somatorio + custos[contador];
+    contador = contador + 1;
+}
+console.log( "o seu custo mensal é de " + somatorio);
+
+
+let salario = [4000,3800];
+contador = 0;
+let somatorioSalario = 0;
+while (contador<salario.length){
+    somatorioSalario = somatorioSalario + salario[contador];
+    contador = contador + 1;
+}
+console.log ("Sua renda mensal é de " + somatorioSalario);
